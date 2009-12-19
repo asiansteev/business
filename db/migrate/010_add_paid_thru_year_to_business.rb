8a00de02bdcbbf1a778d4b7120b088ea615ce21e
Steven Hayes <steev@steven-laptop.(none)>
2009-12-19 13:09:02 -0500

    Default paid thru year should be 2009.

--- db/migrate/010_add_paid_thru_year_to_business.rb
+++ db/migrate/010_add_paid_thru_year_to_business.rb
@@ -1,6 +1,6 @@
 class AddPaidThruYearToBusiness < ActiveRecord::Migration
   def self.up
-    add_column :businesses, :paid_thru_year, :int
+    add_column :businesses, :paid_thru_year, :int, :default => 2009
   end
   def self.down
     remove_column :businesses, :paid_thru_year
